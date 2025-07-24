9c1f2cab356a3855cbf289c18a0c2d8cf93b05b6
Yagzie <97476482+Yagzie@users.noreply.github.com>
2025-07-25 01:03:27 +0300

    target: *: miscs: fix-up of a serious issue Somehow using the default UN1CA script causes issues that it corrupts the build.prop to 0B; which is not a thing we want. This commit is for fixing it.

--- target/a73xq/patches/miscs/customize.sh
+++ target/a73xq/patches/miscs/customize.sh
@@ -1,5 +1,5 @@
-echo "Fix up /product/etc/build.prop"
+LOG_STEP_IN "- Fix up /product/etc/build.prop"
 sed -i "/# Removed by /d" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "s/#bluetooth./bluetooth./g" "$WORK_DIR/product/etc/build.prop" \
-    && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop" \
-    && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/product/etc/build.prop"
+    && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop"
+LOG_STEP_OUT
