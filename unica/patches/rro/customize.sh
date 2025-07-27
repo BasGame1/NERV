07b79c72b7d5db5dd61d3661a1cc37a1e195a327
utkustnr <utkuustuner@proton.me>
2025-07-27 14:48:36 +0300

    unica: rro: don't add overlay if target=source
    Signed-off-by: utkustnr <utkuustuner@proton.me>

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -1,9 +1,11 @@
-if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "product" "overlay/framework-res__a73xqxx__auto_generated_rro_product.apk"
+if [[ $TARGET_FIRMWARE != $SOURCE_FIRMWARE ]]; then
+    if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
+        DECODE_APK "product" "overlay/framework-res__a73xqxx__auto_generated_rro_product.apk"
 
-    echo "Applying stock overlay configs"
-    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__a73xqxx__auto_generated_rro_product.apk/res"
-    cp -a --preserve=all \
-        "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
-        "$APKTOOL_DIR/product/overlay/framework-res__a73xqxx__auto_generated_rro_product.apk/res"
+        echo "Applying stock overlay configs"
+        rm -rf "$APKTOOL_DIR/product/overlay/framework-res__a73xqxx__auto_generated_rro_product.apk/res"
+        cp -a --preserve=all \
+            "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
+            "$APKTOOL_DIR/product/overlay/framework-res__a73xqxx__auto_generated_rro_product.apk/res"
+    fi
 fi
