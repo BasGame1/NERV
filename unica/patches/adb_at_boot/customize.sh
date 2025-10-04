1f3e6d97531268fa70d0d16baeeef89a7d2c3cdd
BasGame1 <abadbasantaalex@gmail.com>
2025-10-04 12:05:23 +0200

    unica: patches: adb_at_boot: init

---
+++ unica/patches/adb_at_boot/customize.sh
@@ -0,0 +1,18 @@
+# Start adbd on boot
+# https://android.googlesource.com/platform/packages/modules/adb/+/refs/heads/main/docs/dev/how_adbd_starts.md
+SET_PROP_IF_DIFF "product" "persist.sys.usb.config" "$(GET_PROP "product" "persist.sys.usb.config"),adb"
+SET_PROP_IF_DIFF "odm" "persist.sys.usb.config" "$(GET_PROP "odm" "persist.sys.usb.config"),adb"
+SET_PROP_IF_DIFF "odm_dlkm" "persist.sys.usb.config" "$(GET_PROP "odm_dlkm" "persist.sys.usb.config"),adb"
+SET_PROP_IF_DIFF "system_dlkm" "persist.sys.usb.config" "$(GET_PROP "system_dlkm" "persist.sys.usb.config"),adb"
+SET_PROP_IF_DIFF "vendor" "persist.sys.usb.config" "$(GET_PROP "vendor" "persist.sys.usb.config"),adb"
+SET_PROP_IF_DIFF "vendor_dlkm" "persist.sys.usb.config" "$(GET_PROP "vendor_dlkm" "persist.sys.usb.config"),adb"
+
+if [ -f "$WORK_DIR/vendor/etc/init/hw/init.target.rc" ]; then
+    if ! grep -q "persist.vendor.radio.port_index" "$WORK_DIR/vendor/etc/init/hw/init.target.rc"; then
+        {
+            echo ""
+            echo "on property:persist.vendor.radio.port_index=\"\""
+            echo "    setprop sys.usb.config adb"
+        } >> "$WORK_DIR/vendor/etc/init/hw/init.target.rc"
+    fi
+fi
