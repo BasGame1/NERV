e5aa0388064cdf69cc1d6097423e82620eb065a6
BasGame1 <abadbasantaalex@gmail.com>
2025-10-04 12:08:33 +0200

    unica: mods: bootanim: customize: give support for 720p resolution

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -1,10 +1,12 @@
 TWOTHREE_TARGETS=""
-TWOFOUR_TARGETS="a52sxq a51 a53x a73xq m52xq r8q r9q r9q2"
+TWOFOUR_TARGETS=""
+ONETWO_TARGETS="a21s"
 
-if echo "$TWOTHREE_TARGETS" | grep -q -w "$TARGET_CODENAME"; then
+if echo "Screen resolution 2340x1080 detected for $TARGET_CODENAME"; then
     cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/2340x1080/"* "$WORK_DIR/system/system/media"
-elif echo "$TWOFOUR_TARGETS" | grep -q -w "$TARGET_CODENAME"; then
+elif echo "Screen resolution 2400x1080 detected for $TARGET_CODENAME"; then
     cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/2400x1080/"* "$WORK_DIR/system/system/media"
+elif echo "Screen resolution 1280x720 detected for $TARGET_CODENAME"; then cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/1280x720/"* "$WORK_DIR/system/system/media"
 else
     echo "Unknown boot animation resolution for \"$TARGET_CODENAME\""
 fi
